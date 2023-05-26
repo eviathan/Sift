b3585fea3b80fab3240297f7ad9d73f872cd6b7f
Brian Williams <brian.williams@lbresearch.com>
2023-05-26 01:08:32 +0100

    Add some reminder notes

--- Services/MIDIService.cs
+++ Services/MIDIService.cs
@@ -6,6 +6,8 @@ public static class MIDIService
     {
         // Get all MIDI output devices
         var midiAccess = MidiAccessManager.Default;
+
+        // TODO: THIS NEEDS TO BE SET THROUGH THE UI (and or through a virtual MIDI port) at the moment its sort of hardwired to what ever hardware synth I enable
         var outputDevice = midiAccess.Outputs.ElementAt(1);
 
         // Find first output device
@@ -19,7 +21,7 @@ public static class MIDIService
         // Open the device and send MIDI message
         var midiOutput = await midiAccess.OpenOutputAsync(outputDevice.Id);
         midiOutput.Send(new byte[] { 0x90, (byte)note, 0x7F }, 0, 3, 0); // Note on
-        await Task.Delay(200);
+        await Task.Delay(200); // TODO: This needs be determinned by the note length/ or a better way than just chewing through threads
         midiOutput.Send(new byte[] { 0x80, (byte)note, 0x7F }, 0, 3, 0); // Note off
     }
 }
